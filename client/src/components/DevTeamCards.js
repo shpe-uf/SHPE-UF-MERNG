219473128f0c1949e542af23c7643288e055fcc2
cecrigope <cecrigope@hotmail.com>
2019-10-19 17:00:41 -0400

    Added pictures to the about us page

--- client/src/components/DevTeamCards.js
+++ client/src/components/DevTeamCards.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
 
-import alejandro from "../assets/images/team/2019-2020/alejandro.JPG";
-import cesar from "../assets/images/team/2019-2020/cesar.JPG";
-import david from "../assets/images/team/2019-2020/david.JPG";
-import diego from "../assets/images/team/2019-2020/diego.JPG";
-import eduardo from "../assets/images/team/2019-2020/eduardo.JPG";
-import isabel from "../assets/images/team/2019-2020/isabel.JPG";
-import juan from "../assets/images/team/2019-2020/juan.JPG";
-import mariana from "../assets/images/team/2019-2020/mariana.JPG";
+import alejandro from "../assets/images/team/2019-2020/alejandro.jpg";
+import cesar from "../assets/images/team/2019-2020/cesar.jpg";
+import david from "../assets/images/team/2019-2020/david.jpg";
+import diego from "../assets/images/team/2019-2020/diego.jpg";
+import eduardo from "../assets/images/team/2019-2020/eduardo.jpg";
+import isabel from "../assets/images/team/2019-2020/isabel.jpg";
+import juan from "../assets/images/team/2019-2020/juan.jpg";
+import mariana from "../assets/images/team/2019-2020/mariana.jpg";
 import placeholder from "../assets/images/team/placeholder.png";
 
 
