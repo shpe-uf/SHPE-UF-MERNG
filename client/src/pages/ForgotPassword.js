80230f0f96790282c31a9cae4805c4f2dbf8531d
Eddy98 <eduardodgraziano@ufl.edu>
2019-10-20 16:57:48 -0400

    responsiveness fixed

--- client/src/pages/ForgotPassword.js
+++ client/src/pages/ForgotPassword.js
@@ -1,14 +1,12 @@
 import React, { useState, useContext } from "react";
-import { Form, Button, Container, Segment, Grid } from "semantic-ui-react";
+import { Form, Button, Container, Segment, Grid, Responsive } from "semantic-ui-react";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
-import { AuthContext } from "../context/auth";
 
 import { useForm } from "../util/hooks";
 
 function ForgotPassword(props){
 
-  const context = useContext(AuthContext);
   const [errors, setErrors] = useState({});
 
   const { onChange, onSubmit, values } = useForm(resetCallback, {
@@ -33,48 +31,138 @@ function ForgotPassword(props){
   return (
     <div className="reset">
       <div className="overlay-reset">
-        <Container>
-          <Grid>
-            <Grid.Row centered>
-              <Grid.Column width={6}>
-                <Segment.Group>
-                  <Segment className="title-bg-accent-1">
-                    <h1 className="text-white">Reset Password</h1>
-                  </Segment>
-                  <Segment>
-                    {Object.keys(errors).length > 0 && (
-                      <div className="ui error message">
-                        <ul className="list">
-                          {Object.values(errors).map(value => (
-                            <li key={value}>{value}</li>
-                          ))}
-                        </ul>
-                      </div>
-                    )}
-                    <Form
-                      onSubmit={onSubmit}
-                      noValidate
-                    >
-                      <Form.Input
-                        type="text"
-                        label="Email"
-                        name="email"
-                        value={values.email}
-                        onChange={onChange}
-                      />
-                      <span>
-                        <Button type="submit">Reset Password</Button>
-                        <p style={{display : 'inline-block', marginTop: 12, marginLeft: 10}}>
-                          or <a href="/login">Log In</a>
-                        </p>
-                      </span>
-                    </Form>
-                  </Segment>
-                </Segment.Group>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
+        <Responsive {...Responsive.onlyComputer}>
+          <Container>
+            <Grid>
+              <Grid.Row centered>
+                <Grid.Column width={6}>
+                  <Segment.Group>
+                    <Segment className="title-bg-accent-1">
+                      <h1 className="text-white">Reset Password</h1>
+                    </Segment>
+                    <Segment>
+                      {Object.keys(errors).length > 0 && (
+                        <div className="ui error message">
+                          <ul className="list">
+                            {Object.values(errors).map(value => (
+                              <li key={value}>{value}</li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
+                      <Form
+                        onSubmit={onSubmit}
+                        noValidate
+                      >
+                        <Form.Input
+                          type="text"
+                          label="Email"
+                          name="email"
+                          value={values.email}
+                          onChange={onChange}
+                        />
+                        <span>
+                          <Button type="submit">Reset Password</Button>
+                          <p className="resetLink">
+                            or <a href="/login">Log In</a>
+                          </p>
+                        </span>
+                      </Form>
+                    </Segment>
+                  </Segment.Group>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Responsive>
+        <Responsive {...Responsive.onlyTablet}>
+          <Container>
+            <Grid>
+              <Grid.Row centered>
+                <Grid.Column width={12}>
+                  <Segment.Group>
+                    <Segment className="title-bg-accent-1">
+                      <h1 className="text-white">Reset Password</h1>
+                    </Segment>
+                    <Segment>
+                      {Object.keys(errors).length > 0 && (
+                        <div className="ui error message">
+                          <ul className="list">
+                            {Object.values(errors).map(value => (
+                              <li key={value}>{value}</li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
+                      <Form
+                        onSubmit={onSubmit}
+                        noValidate
+                      >
+                        <Form.Input
+                          type="text"
+                          label="Email"
+                          name="email"
+                          value={values.email}
+                          onChange={onChange}
+                        />
+                        <span>
+                          <Button type="submit">Reset Password</Button>
+                          <p className="resetLink">
+                            or <a href="/login">Log In</a>
+                          </p>
+                        </span>
+                      </Form>
+                    </Segment>
+                  </Segment.Group>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Responsive>
+        <Responsive {...Responsive.onlyMobile}>
+          <Container>
+            <Grid>
+              <Grid.Row centered>
+                <Grid.Column width={16}>
+                  <Segment.Group>
+                    <Segment className="title-bg-accent-1">
+                      <h1 className="text-white">Reset Password</h1>
+                    </Segment>
+                    <Segment>
+                      {Object.keys(errors).length > 0 && (
+                        <div className="ui error message">
+                          <ul className="list">
+                            {Object.values(errors).map(value => (
+                              <li key={value}>{value}</li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
+                      <Form
+                        onSubmit={onSubmit}
+                        noValidate
+                      >
+                        <Form.Input
+                          type="text"
+                          label="Email"
+                          name="email"
+                          value={values.email}
+                          onChange={onChange}
+                        />
+                        <span>
+                          <Button type="submit">Reset Password</Button>
+                          <p className="resetLink">
+                            or <a href="/login">Log In</a>
+                          </p>
+                        </span>
+                      </Form>
+                    </Segment>
+                  </Segment.Group>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Responsive>
       </div>
     </div>
   );
--- client/src/pages/ResetPassword.js
+++ client/src/pages/ResetPassword.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button, Container, Segment, Grid } from "semantic-ui-react";
+import { Form, Button, Container, Segment, Grid, Responsive } from "semantic-ui-react";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
 
@@ -33,55 +33,159 @@ function ResetPassword(props){
   return (
     <div className="reset">
       <div className="overlay-reset">
-        <Container>
-          <Grid>
-            <Grid.Row centered>
-              <Grid.Column width={6}>
-                <Segment.Group>
-                  <Segment className="title-bg-accent-1">
-                    <h1 className="text-white">Reset Password</h1>
-                  </Segment>
-                  <Segment>
-                    {Object.keys(errors).length > 0 && (
-                      <div className="ui error message">
-                        <ul className="list">
-                          {Object.values(errors).map(value => (
-                            <li key={value}>{value}</li>
-                          ))}
-                        </ul>
-                      </div>
-                    )}
-                    <Form
-                      onSubmit={onSubmit}
-                      noValidate
-                    >
-                      <Form.Input
-                        type="password"
-                        label="Password"
-                        name="password"
-                        value={values.password}
-                        onChange={onChange}
-                      />
-                      <Form.Input
-                        type="password"
-                        label="Confirm Password"
-                        name="confirmPassword"
-                        value={values.confirmPassword}
-                        onChange={onChange}
-                      />
-                      <span>
-                        <Button type="submit">Submit</Button>
-                        <p style={{display : 'inline-block', marginTop: 12, marginLeft: 10}}>
-                          or <a href="/login">Log In</a>
-                        </p>
-                      </span>
-                    </Form>
-                  </Segment>
-                </Segment.Group>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
+        <Responsive {...Responsive.onlyComputer}>
+          <Container>
+            <Grid>
+              <Grid.Row centered>
+                <Grid.Column width={6}>
+                  <Segment.Group>
+                    <Segment className="title-bg-accent-1">
+                      <h1 className="text-white">Reset Password</h1>
+                    </Segment>
+                    <Segment>
+                      {Object.keys(errors).length > 0 && (
+                        <div className="ui error message">
+                          <ul className="list">
+                            {Object.values(errors).map(value => (
+                              <li key={value}>{value}</li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
+                      <Form
+                        onSubmit={onSubmit}
+                        noValidate
+                      >
+                        <Form.Input
+                          type="password"
+                          label="Password"
+                          name="password"
+                          value={values.password}
+                          onChange={onChange}
+                        />
+                        <Form.Input
+                          type="password"
+                          label="Confirm Password"
+                          name="confirmPassword"
+                          value={values.confirmPassword}
+                          onChange={onChange}
+                        />
+                        <span>
+                          <Button type="submit">Submit</Button>
+                          <p className="resetLink">
+                            or <a href="/login">Log In</a>
+                          </p>
+                        </span>
+                      </Form>
+                    </Segment>
+                  </Segment.Group>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Responsive>
+        <Responsive {...Responsive.onlyTablet}>
+          <Container>
+            <Grid>
+              <Grid.Row centered>
+                <Grid.Column width={12}>
+                  <Segment.Group>
+                    <Segment className="title-bg-accent-1">
+                      <h1 className="text-white">Reset Password</h1>
+                    </Segment>
+                    <Segment>
+                      {Object.keys(errors).length > 0 && (
+                        <div className="ui error message">
+                          <ul className="list">
+                            {Object.values(errors).map(value => (
+                              <li key={value}>{value}</li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
+                      <Form
+                        onSubmit={onSubmit}
+                        noValidate
+                      >
+                        <Form.Input
+                          type="password"
+                          label="Password"
+                          name="password"
+                          value={values.password}
+                          onChange={onChange}
+                        />
+                        <Form.Input
+                          type="password"
+                          label="Confirm Password"
+                          name="confirmPassword"
+                          value={values.confirmPassword}
+                          onChange={onChange}
+                        />
+                        <span>
+                          <Button type="submit">Submit</Button>
+                          <p className="resetLink">
+                            or <a href="/login">Log In</a>
+                          </p>
+                        </span>
+                      </Form>
+                    </Segment>
+                  </Segment.Group>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Responsive>
+        <Responsive {...Responsive.onlyMobile}>
+          <Container>
+            <Grid>
+              <Grid.Row centered>
+                <Grid.Column width={16}>
+                  <Segment.Group>
+                    <Segment className="title-bg-accent-1">
+                      <h1 className="text-white">Reset Password</h1>
+                    </Segment>
+                    <Segment>
+                      {Object.keys(errors).length > 0 && (
+                        <div className="ui error message">
+                          <ul className="list">
+                            {Object.values(errors).map(value => (
+                              <li key={value}>{value}</li>
+                            ))}
+                          </ul>
+                        </div>
+                      )}
+                      <Form
+                        onSubmit={onSubmit}
+                        noValidate
+                      >
+                        <Form.Input
+                          type="password"
+                          label="Password"
+                          name="password"
+                          value={values.password}
+                          onChange={onChange}
+                        />
+                        <Form.Input
+                          type="password"
+                          label="Confirm Password"
+                          name="confirmPassword"
+                          value={values.confirmPassword}
+                          onChange={onChange}
+                        />
+                        <span>
+                          <Button type="submit">Submit</Button>
+                          <p className="resetLink">
+                            or <a href="/login">Log In</a>
+                          </p>
+                        </span>
+                      </Form>
+                    </Segment>
+                  </Segment.Group>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Responsive>
       </div>
     </div>
   );
