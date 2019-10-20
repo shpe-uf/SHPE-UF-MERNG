bf1d5333f8c02fbc85eae9c8495abf321e772808
Eddy98 <eduardodgraziano@ufl.edu>
2019-10-20 16:13:48 -0400

    input forms smaller

--- client/src/pages/ForgotPassword.js
+++ client/src/pages/ForgotPassword.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { Form, Button, Container, Segment } from "semantic-ui-react";
+import { Form, Button, Container, Segment, Grid } from "semantic-ui-react";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
 import { AuthContext } from "../context/auth";
@@ -34,40 +34,46 @@ function ForgotPassword(props){
     <div className="reset">
       <div className="overlay-reset">
         <Container>
-          <Segment.Group>
-          <Segment className="title-bg-accent-1">
-            <h1 className="text-white">Reset Password</h1>
-          </Segment>
-          <Segment>
-            {Object.keys(errors).length > 0 && (
-              <div className="ui error message">
-                <ul className="list">
-                  {Object.values(errors).map(value => (
-                    <li key={value}>{value}</li>
-                  ))}
-                </ul>
-              </div>
-            )}
-            <Form
-              onSubmit={onSubmit}
-              noValidate
-            >
-              <Form.Input
-                type="text"
-                label="Email"
-                name="email"
-                value={values.email}
-                onChange={onChange}
-              />
-              <span>
-                <Button type="submit">Reset Password</Button>
-                <p style={{display : 'inline-block', marginTop: 12, marginLeft: 10}}>
-                  or <a href="/login">Log In</a>
-                </p>
-              </span>
-            </Form>
-          </Segment>
-          </Segment.Group>
+          <Grid>
+            <Grid.Row centered>
+              <Grid.Column width={6}>
+                <Segment.Group>
+                  <Segment className="title-bg-accent-1">
+                    <h1 className="text-white">Reset Password</h1>
+                  </Segment>
+                  <Segment>
+                    {Object.keys(errors).length > 0 && (
+                      <div className="ui error message">
+                        <ul className="list">
+                          {Object.values(errors).map(value => (
+                            <li key={value}>{value}</li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
+                    <Form
+                      onSubmit={onSubmit}
+                      noValidate
+                    >
+                      <Form.Input
+                        type="text"
+                        label="Email"
+                        name="email"
+                        value={values.email}
+                        onChange={onChange}
+                      />
+                      <span>
+                        <Button type="submit">Reset Password</Button>
+                        <p style={{display : 'inline-block', marginTop: 12, marginLeft: 10}}>
+                          or <a href="/login">Log In</a>
+                        </p>
+                      </span>
+                    </Form>
+                  </Segment>
+                </Segment.Group>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </Container>
       </div>
     </div>
--- client/src/pages/ResetPassword.js
+++ client/src/pages/ResetPassword.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button, Container, Segment } from "semantic-ui-react";
+import { Form, Button, Container, Segment, Grid } from "semantic-ui-react";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
 
@@ -34,47 +34,53 @@ function ResetPassword(props){
     <div className="reset">
       <div className="overlay-reset">
         <Container>
-          <Segment.Group>
-          <Segment className="title-bg-accent-1">
-            <h1 className="text-white">Reset Password</h1>
-          </Segment>
-          <Segment>
-            {Object.keys(errors).length > 0 && (
-              <div className="ui error message">
-                <ul className="list">
-                  {Object.values(errors).map(value => (
-                    <li key={value}>{value}</li>
-                  ))}
-                </ul>
-              </div>
-            )}
-            <Form
-              onSubmit={onSubmit}
-              noValidate
-            >
-              <Form.Input
-                type="password"
-                label="Password"
-                name="password"
-                value={values.password}
-                onChange={onChange}
-              />
-              <Form.Input
-                type="password"
-                label="Confirm Password"
-                name="confirmPassword"
-                value={values.confirmPassword}
-                onChange={onChange}
-              />
-              <span>
-                <Button type="submit">Submit</Button>
-                <p style={{display : 'inline-block', marginTop: 12, marginLeft: 10}}>
-                  or <a href="/login">Log In</a>
-                </p>
-              </span>
-            </Form>
-          </Segment>
-          </Segment.Group>
+          <Grid>
+            <Grid.Row centered>
+              <Grid.Column width={6}>
+                <Segment.Group>
+                  <Segment className="title-bg-accent-1">
+                    <h1 className="text-white">Reset Password</h1>
+                  </Segment>
+                  <Segment>
+                    {Object.keys(errors).length > 0 && (
+                      <div className="ui error message">
+                        <ul className="list">
+                          {Object.values(errors).map(value => (
+                            <li key={value}>{value}</li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
+                    <Form
+                      onSubmit={onSubmit}
+                      noValidate
+                    >
+                      <Form.Input
+                        type="password"
+                        label="Password"
+                        name="password"
+                        value={values.password}
+                        onChange={onChange}
+                      />
+                      <Form.Input
+                        type="password"
+                        label="Confirm Password"
+                        name="confirmPassword"
+                        value={values.confirmPassword}
+                        onChange={onChange}
+                      />
+                      <span>
+                        <Button type="submit">Submit</Button>
+                        <p style={{display : 'inline-block', marginTop: 12, marginLeft: 10}}>
+                          or <a href="/login">Log In</a>
+                        </p>
+                      </span>
+                    </Form>
+                  </Segment>
+                </Segment.Group>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </Container>
       </div>
     </div>
