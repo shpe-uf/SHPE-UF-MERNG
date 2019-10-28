9228ebe2d017f53d31e0deef8b029b3ca0301fe5
isamitre <isamitre@gmail.com>
2019-10-28 01:39:24 -0400

    EOD, working on letting users see all possible tasks

--- client/src/pages/Points.js
+++ client/src/pages/Points.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Grid, Container, Button, Modal, Form } from "semantic-ui-react";
+import { Grid, Container, Card, Menu, Segment, Button, Modal, Form } from "semantic-ui-react";
 import gql from "graphql-tag";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 
@@ -10,7 +10,15 @@ import Title from "../components/Title";
 import PointsBar from "../components/PointsBar";
 import PointsTable from "../components/PointsTable";
 
+import { FETCH_TASKS_QUERY } from "../util/graphql";
+
 function Points() {
+  const [activeItem, setActiveItem] = useState("Your Points");
+
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+  };
+
   const [errors, setErrors] = useState({});
   var getUser = "";
 
@@ -78,6 +86,7 @@ function Points() {
     getUser.springPoints = userData.springPoints;
     getUser.summerPoints = userData.summerPoints;
     getUser.events = userData.events;
+    getUser.tasks = userData.tasks;
     getUser.message = userData.message;
   }
 
@@ -86,78 +95,124 @@ function Points() {
       <Title title="Points Program" />
 
       <Container>
-        <Grid stackable>
-          {getUser && getUser.message && getUser.message !== undefined && (
-            <Grid.Row>
-              <Grid.Column>
-                <div className="ui warning message">
-                  <p>{getUser.message}</p>
-                </div>
-              </Grid.Column>
-            </Grid.Row>
-          )}
-          <Grid.Row>
-            <Grid.Column>
-              <Button
-                content="Redeem Code"
-                icon="font"
-                labelPosition="left"
-                floated="right"
-                onClick={() => openModal("redeemPoints")}
-              />
-            </Grid.Column>
-          </Grid.Row>
-          {getUser && (
-            <>
-              <PointsBar user={getUser} />
-              <PointsTable user={getUser} />
-            </>
-          )}
-        </Grid>
-      </Container>
+        <Menu attached="top" tabular>
+          <Menu.Item
+            name="Your Points"
+            active={activeItem === "Your Points"}
+            onClick={handleItemClick}
+          />
+          <Menu.Item
+            name="Tasks"
+            active={activeItem === "Tasks"}
+            onClick={handleItemClick}
+          />
+        </Menu>
 
-      <Modal open={redeemPointsModal} size="tiny">
-        <Modal.Header>
-          <h2>Redeem Points</h2>
-        </Modal.Header>
-        <Modal.Content>
-          <Modal.Description>
-            {Object.keys(errors).length > 0 && (
-              <div className="ui error message">
-                <ul className="list">
-                  {Object.values(errors).map(value => (
-                    <li key={value}>{value}</li>
-                  ))}
-                </ul>
-              </div>
-            )}
-            <Form
-              onSubmit={onSubmit}
-              noValidate
-              className={loading ? "loading" : ""}
-            >
-              <Form.Input
-                type="text"
-                label="Event Code"
-                name="code"
-                value={values.code}
-                error={errors.code ? true : false}
-                onChange={onChange}
-              />
-              <Button
-                type="reset"
-                color="grey"
-                onClick={() => closeModal("redeemPoints")}
-              >
-                Cancel
-              </Button>
-              <Button type="submit" floated="right">
-                Submit
-              </Button>
-            </Form>
-          </Modal.Description>
-        </Modal.Content>
-      </Modal>
+        {activeItem === "Your Points" && (
+          <Segment attached="bottom">
+            <Grid stackable>
+              {getUser && getUser.message && getUser.message !== undefined && (
+                <Grid.Row>
+                  <Grid.Column>
+                    <div className="ui warning message">
+                      <p>{getUser.message}</p>
+                    </div>
+                  </Grid.Column>
+                </Grid.Row>
+              )}
+              <Grid.Row>
+                <Grid.Column>
+                  <Button
+                    content="Redeem Code"
+                    icon="font"
+                    labelPosition="left"
+                    floated="right"
+                    onClick={() => openModal("redeemPoints")}
+                  />
+                </Grid.Column>
+              </Grid.Row>
+              {getUser && (
+                <>
+                  <PointsBar user={getUser} />
+                  <PointsTable user={getUser} />
+                </>
+              )}
+            </Grid>
+
+            <Modal open={redeemPointsModal} size="tiny">
+              <Modal.Header>
+                <h2>Redeem Points</h2>
+              </Modal.Header>
+              <Modal.Content>
+                <Modal.Description>
+                  {Object.keys(errors).length > 0 && (
+                    <div className="ui error message">
+                      <ul className="list">
+                        {Object.values(errors).map(value => (
+                          <li key={value}>{value}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  )}
+                  <Form
+                    onSubmit={onSubmit}
+                    noValidate
+                    className={loading ? "loading" : ""}
+                  >
+                    <Form.Input
+                      type="text"
+                      label="Event Code"
+                      name="code"
+                      value={values.code}
+                      error={errors.code ? true : false}
+                      onChange={onChange}
+                    />
+                    <Button
+                      type="reset"
+                      color="grey"
+                      onClick={() => closeModal("redeemPoints")}
+                    >
+                      Cancel
+                    </Button>
+                    <Button type="submit" floated="right">
+                      Submit
+                    </Button>
+                  </Form>
+                </Modal.Description>
+              </Modal.Content>
+            </Modal>
+          </Segment>
+        )}
+        {activeItem === "Tasks" && (
+          <Segment attached="bottom">
+            <Grid stackable>
+              {getUser && getUser.message && getUser.message !== undefined && (
+                <Grid.Row>
+                  <Grid.Column>
+                    <div className="ui warning message">
+                      <p>{getUser.message}</p>
+                    </div>
+                  </Grid.Column>
+                </Grid.Row>
+              )}
+              <Card.Group>
+              {tasks && tasks.map((task, index) => (
+                <Card>
+                  <Card.Content>
+                    <Card.Header floated='right'>{task.points}</Card.Header>
+                    <Card.Header>{task.name}</Card.Header>
+                    <Card.Meta>{task.startDate}   {task.endDate}</Card.Meta>
+                    <Card.Description>{task.description}</Card.Description>
+                  </Card.Content>
+                </Card>
+              ))}
+              </Card.Group>
+            </Grid>
+
+
+          </Segment>
+        )}
+      </Container>
     </div>
   );
 }
@@ -175,6 +230,13 @@ const FETCH_USER_QUERY = gql`
         name
         category
         createdAt
+        points
+      }
+      tasks {
+        name
+        startDate
+        endDate
+        description
         points
       }
     }
