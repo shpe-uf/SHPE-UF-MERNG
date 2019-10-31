ee110c034bed07b937cb737456810ca8a64e7095
Eddy98 <eduardodgraziano@ufl.edu>
2019-10-31 14:33:44 -0400

    done

--- client/src/pages/Confirm.js
+++ client/src/pages/Confirm.js
@@ -1,19 +1,16 @@
 import React, { useState, useEffect } from "react";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
-import { Segment, Dimmer, Loader, Container, Grid } from "semantic-ui-react";
+import { Segment, Dimmer, Loader, Container, Grid, Responsive } from "semantic-ui-react";
 
 function Confirm(props){
   const [confirming, setConfirming] = useState(true);
-  const [loading, setLoading] = useState("active");
-
 
   const value  = {
     id: props.match.params.id
   };
 
   const [confirm] = useMutation(CONFIRM_USER, {
-
     onCompleted(){
       setConfirming(false);
     },
@@ -25,36 +22,81 @@ function Confirm(props){
   });
 
   return(
+
     <div className="login">
       <div className="overlay-confirm">
-        <Container>
-          <Grid>
-            <Grid.Row centered>
-              <Grid.Column width={8}>
-                <Segment.Group>
-                  <Segment className="title-bg-accent-1">
-                    <div className="loading">
-                      <Dimmer active={confirming} inverted>
-                        <Loader size='massive' disabled={!confirming}>Loading</Loader>
-                      </Dimmer>
-                      {!confirming && (
-                        <div>
-                          <h1 className="text-white confirmMsg">
-                            Your email is confirmed
-                          </h1>
-                        </div>
-                      )}
-                    </div>
-                  </Segment>
-                </Segment.Group>
-
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
+        <Dimmer active={confirming} inverted>
+          <Loader size='massive' disabled={!confirming}>Loading</Loader>
+        </Dimmer>
+        {!confirming && (
+          <>
+            <Responsive {...Responsive.onlyComputer}>
+              <Container>
+                <Grid>
+                  <Grid.Row centered>
+                    <Grid.Column width={8}>
+                      <Segment.Group>
+                        <Segment className="title-bg-accent-1">
+                          <div className="loading">
+                              <div>
+                                <h1 className="text-white confirmMsg">
+                                  Your email is confirmed!
+                                </h1>
+                              </div>
+                          </div>
+                        </Segment>
+                      </Segment.Group>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Container>
+            </Responsive>
+            <Responsive {...Responsive.onlyTablet}>
+              <Container>
+                <Grid>
+                  <Grid.Row centered>
+                    <Grid.Column width={12}>
+                      <Segment.Group>
+                        <Segment className="title-bg-accent-1">
+                          <div className="loading">
+                              <div>
+                                <h1 className="text-white confirmMsg">
+                                  Your email is confirmed!
+                                </h1>
+                              </div>
+                          </div>
+                        </Segment>
+                      </Segment.Group>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Container>
+            </Responsive>
+            <Responsive {...Responsive.onlyMobile}>
+              <Container>
+                <Grid>
+                  <Grid.Row centered>
+                    <Grid.Column width={16}>
+                      <Segment.Group>
+                        <Segment className="title-bg-accent-1">
+                          <div className="loading">
+                              <div>
+                                <h1 className="text-white confirmMsg">
+                                  Your email is confirmed!
+                                </h1>
+                              </div>
+                          </div>
+                        </Segment>
+                      </Segment.Group>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              </Container>
+            </Responsive>
+          </>
+        )}
       </div>
     </div>
-
   );
 
 }
