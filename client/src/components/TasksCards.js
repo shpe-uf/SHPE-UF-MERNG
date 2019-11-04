78053354047c59f9d1b011df20004b7d5c100b9e
isamitre <isamitre@gmail.com>
2019-11-04 17:34:31 -0500

    added message for when there's no tasks

--- client/src/components/TasksCards.js
+++ client/src/components/TasksCards.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Card } from "semantic-ui-react";
+import { Dimmer, Loader, Segment, Header, Grid, Card } from "semantic-ui-react";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 
 import { FETCH_TASKS_QUERY } from "../util/graphql";
@@ -8,29 +8,43 @@ function TasksCards({ user }) {
   var tasks = useQuery(FETCH_TASKS_QUERY).data.getTasks;
 
   return (
-    <Card.Group itemsPerRow={3}>
-      {tasks &&
-        tasks.map((task, index) => (
-          <Card color="blue">
-            <Card.Content>
-              <Grid.Row itemsPerRow={2}>
-                <Grid.Column>
-                  <Card.Header>{task.name}</Card.Header>
-                </Grid.Column>
-                <Grid.Column>
-                  <Card.Header textAlign="right">{task.points}</Card.Header>
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
-                <Card.Meta>
-                  {task.startDate} - {task.endDate}
-                </Card.Meta>
-                <Card.Description>{task.description}</Card.Description>
-              </Grid.Row>
-            </Card.Content>
-          </Card>
-        ))}
-    </Card.Group>
+    <>
+      <Dimmer active={tasks ? false : true} inverted>
+        <Loader />
+      </Dimmer>
+      {tasks === undefined || tasks.length === 0 ? (
+        <Segment placeholder>
+          <Header icon>
+            <i className="fas fa-inbox"></i>
+            <p>It seems like there are no tasks at this moment.</p>
+          </Header>
+        </Segment>
+      ) : (
+        <Card.Group itemsPerRow={3}>
+          {tasks &&
+            tasks.map((task, index) => (
+              <Card color="blue">
+                <Card.Content>
+                  <Grid.Row itemsPerRow={2}>
+                    <Grid.Column>
+                      <Card.Header>{task.name}</Card.Header>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Card.Header textAlign="right">{task.points}</Card.Header>
+                    </Grid.Column>
+                  </Grid.Row>
+                  <Grid.Row>
+                    <Card.Meta>
+                      {task.startDate} - {task.endDate}
+                    </Card.Meta>
+                    <Card.Description>{task.description}</Card.Description>
+                  </Grid.Row>
+                </Card.Content>
+              </Card>
+            ))}
+        </Card.Group>
+      )}
+    </>
   );
 }
 
