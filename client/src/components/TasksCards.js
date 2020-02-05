7b9e6f5524a4261e5c3181fa3824a01904d7644b
Eddy98 <eduardodgraziano@ufl.edu>
2020-02-04 19:56:31 -0500

    mobile fix

--- client/src/components/TasksCards.js
+++ client/src/components/TasksCards.js
@@ -99,6 +99,41 @@ function TasksCards({user}) {
             }
           </Card.Group>
         </Responsive>
+        <Responsive {...Responsive.onlyTablet}>
+          <Card.Group itemsPerRow={2}>
+            {
+              tasks && tasks.map((task, index) => (<Card color="blue" key={index}>
+                <Card.Content>
+                  <h5 style={{
+                      float: "left"
+                    }}>{task.name}</h5>
+                  <b>
+                    <p style={{
+                        float: "right"
+                      }}>Point(s): {task.points}</p>
+                  </b>
+                  <Card.Meta style={{
+                      clear: "left"
+                    }}>{task.startDate}
+                    - {task.endDate}</Card.Meta>
+                </Card.Content>
+                <Card.Content>{task.description}</Card.Content>
+                <Card.Content>
+                  <Button fluid="fluid" basic="basic" color="green" onClick={() => {
+                      redeemTasksPoints({
+                        variables: {
+                          name: task.name,
+                          username: username
+                        }
+                      });
+                    }}>
+                    Request
+                  </Button>
+                </Card.Content>
+              </Card>))
+            }
+          </Card.Group>
+        </Responsive>
         <Responsive {...Responsive.onlyMobile}>
           <Card.Group itemsPerRow={1}>
             {
